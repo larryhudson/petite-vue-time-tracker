b585f0bc197a1a89bb3ac914a5d611c656cc4fe5
Larry Hudson <larryhudson@hey.com>
2023-01-31 17:40:04 +1100

    updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy({
-        'node_modules/petite-vue/dist/petite-vue.es.js': 'petite-vue.es.js'
+        'node_modules/petite-vue/dist/petite-vue.es.js': 'petite-vue.es.js',
+        'node_modules/reasonable-colors/reasonable-colors.css': 'reasonable-colors.css'
     })
 }
